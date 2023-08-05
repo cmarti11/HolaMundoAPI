09d55b790bd80f3a50503181c67b3c0c9977d9d6
Cristian Martinez <107958139+cmartinezcxm@users.noreply.github.com>
2023-08-05 10:53:50 -0500

    Add User, UserRole

--- HolaMundoAPI/HolaMundoAPi.API/Data/DatabaseContext.cs
+++ HolaMundoAPI/HolaMundoAPi.API/Data/DatabaseContext.cs
@@ -14,11 +14,15 @@ namespace HolaMundoAPI.API.Data
         {
         }
 
-        public DbSet<Client> Clients { get; set; } = default!;
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Client>().ToTable(nameof(Client));
+            modelBuilder.Entity<UserRole>().ToTable(nameof(UserRole));
+            modelBuilder.Entity<User>().ToTable(nameof(User));
 
             base.OnModelCreating(modelBuilder);
         }
--- HolaMundoAPI/HolaMundoAPi.API/Migrations/20230805153133_AddUserRole.Designer.cs
+++ HolaMundoAPI/HolaMundoAPi.API/Migrations/20230805153133_AddUserRole.Designer.cs
@@ -3,6 +3,7 @@ using HolaMundoAPI.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HolaMundoAPI.API.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230805153133_AddUserRole")]
+    partial class AddUserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
